507bde9d6e8b138c290852d0a835f0ba02054236
Denis Kezhvatov <kezhvatovdenis@yandex.ru>
2023-02-15 13:24:51 +0300

    Исправлены ошибки по ревью 2

--- scripts/index.js
+++ scripts/index.js
@@ -10,8 +10,6 @@ let jobInput = document.querySelector('.popup__input_field_caption');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
-let popupSubmit = document.querySelector('.popup__submit');
-
 // Функция добавления класса и данных из профиля.
 function openFunc() {
   popupOpened.classList.add('popup_opened');
@@ -23,12 +21,6 @@ function closeFunc() {
   popupOpened.classList.remove('popup_opened');
 }
 
-// Открываем и закрываем попап. Использую объявленную функцию.
-
-profileEditButton.addEventListener('click', openFunc);
-
-popupClosed.addEventListener('click', closeFunc);
-
 // Обработчик «отправки» формы.
 
 function handleFormSubmit(evt) {
@@ -37,7 +29,13 @@ function handleFormSubmit(evt) {
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
 
-  popupSubmit.addEventListener('click', closeFunc);
+  closeFunc();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+// Открываем и закрываем попап. Использую объявленные функции.
+
+profileEditButton.addEventListener('click', openFunc);
+
+popupClosed.addEventListener('click', closeFunc);
